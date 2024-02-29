e3aa7665cb2cfec7658629b859cad0ee42e2a871
Rahil Prakash <54015030+Radrahil@users.noreply.github.com>
2024-02-29 16:01:52 +0530

    Initial commit

---
+++ roundtable/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+  int n = 0;
+  vector<int> input;
+  vector<int> dp;
+  int v = 0;
+
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    cin >> v;
+    input.push_back(v);
+  }
+
+}
