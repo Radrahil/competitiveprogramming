24854f058b616ac47fb868ef10a08520f1becccf
Rahil Prakash <54015030+Radrahil@users.noreply.github.com>
2024-02-29 16:48:03 +0530

    roundtable problem (question is bugged)

--- roundtable/main.cpp
+++ roundtable/main.cpp
@@ -1,18 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main () {
-
-  int n = 0;
-  vector<int> input;
-  vector<int> dp;
-  int v = 0;
-
+int main() {
+  int n;
   cin >> n;
 
+  vector<int> input(n);
   for (int i = 0; i < n; i++) {
-    cin >> v;
-    input.push_back(v);
+    cin >> input[i];
   }
 
+  if (n == 1) {
+    cout << input[0];
+    return 0;
+  }
+
+  vector<long long> dp(n);
+  dp[0] = input[0];
+  dp[1] = input[1];
+  dp[n - 1] = input[n - 1]; // Initialize the last element
+
+  for (int i = 2; i < n - 1; i++) { // Loop only until the second last element
+    dp[i] = min(dp[i - 1], dp[i - 2]) + input[i];
+  }
+
+  long long ans = min(dp[n - 2], dp[n - 3]); // Choose the minimum between the
+                                             // second last and the third last
+
+  cout << ans;
+
+  return 0;
 }
